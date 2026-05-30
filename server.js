ee053b1513abffcc83f33f2e2d22963147734bde
Abdoul David <abdoul.david@outlook.fr>
2026-05-30 01:13:52 -0400

    Fix variables environnement

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
-require('dotenv').config()
+process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
