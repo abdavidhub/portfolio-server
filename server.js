39d14e918da3b5f0852f3328fdb557046b046dce
Abdoul David <abdoul.david@outlook.fr>
2026-05-30 02:03:38 -0400

    Fix CORS production

--- server.js
+++ server.js
@@ -15,7 +15,12 @@ const contactRoutes = require('./routes/contact')
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(cors({ origin: 'http://localhost:5173' }))
+app.use(cors({ 
+  origin: [
+    'http://localhost:5173',
+    'https://portfolio-front-ten-chi.vercel.app/'
+  ]
+   }))
 app.use(express.json())
 app.use('/uploads', express.static('uploads'))
 
